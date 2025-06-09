143e98c924c8b91b478edf2b8ba0402d202b35ac
pandalxb <fzuxx@qq.com>
2025-06-09 14:54:07 +0800

    Add: 1. Integration home page properties 2. Logo of integration

--- integrations/mqtt-device/src/main/java/com/milesight/beaveriot/integrations/mqttdevice/MqttDeviceBootstrap.java
+++ integrations/mqtt-device/src/main/java/com/milesight/beaveriot/integrations/mqttdevice/MqttDeviceBootstrap.java
@@ -1,26 +1,43 @@
 package com.milesight.beaveriot.integrations.mqttdevice;
 
+import com.milesight.beaveriot.base.enums.ErrorCode;
+import com.milesight.beaveriot.base.exception.ServiceException;
+import com.milesight.beaveriot.context.api.CredentialsServiceProvider;
 import com.milesight.beaveriot.context.api.DeviceTemplateServiceProvider;
+import com.milesight.beaveriot.context.api.MqttPubSubServiceProvider;
 import com.milesight.beaveriot.context.integration.bootstrap.IntegrationBootstrap;
+import com.milesight.beaveriot.context.integration.enums.CredentialsType;
+import com.milesight.beaveriot.context.integration.model.Credentials;
 import com.milesight.beaveriot.context.integration.model.DeviceTemplate;
 import com.milesight.beaveriot.context.integration.model.Integration;
+import com.milesight.beaveriot.context.integration.wrapper.AnnotatedEntityWrapper;
+import com.milesight.beaveriot.context.mqtt.model.MqttBrokerInfo;
+import com.milesight.beaveriot.integrations.mqttdevice.entity.MqttDeviceIntegrationEntities;
 import com.milesight.beaveriot.integrations.mqttdevice.service.MqttDeviceMqttService;
 import com.milesight.beaveriot.integrations.mqttdevice.support.DataCenter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
+import java.net.InetAddress;
 import java.util.List;
+import java.util.Map;
 
 /**
  * author: Luxb
  * create: 2025/5/14 13:54
  **/
+@Slf4j
 @Component
 public class MqttDeviceBootstrap implements IntegrationBootstrap {
+    private final MqttPubSubServiceProvider mqttPubSubServiceProvider;
+    private final CredentialsServiceProvider credentialsServiceProvider;
     private final DeviceTemplateServiceProvider deviceTemplateServiceProvider;
     private final MqttDeviceMqttService mqttDeviceMqttService;
 
-    public MqttDeviceBootstrap(DeviceTemplateServiceProvider deviceTemplateServiceProvider, MqttDeviceMqttService mqttDeviceMqttService) {
+    public MqttDeviceBootstrap(MqttPubSubServiceProvider mqttPubSubServiceProvider, CredentialsServiceProvider credentialsServiceProvider, DeviceTemplateServiceProvider deviceTemplateServiceProvider, MqttDeviceMqttService mqttDeviceMqttService) {
+        this.mqttPubSubServiceProvider = mqttPubSubServiceProvider;
+        this.credentialsServiceProvider = credentialsServiceProvider;
         this.deviceTemplateServiceProvider = deviceTemplateServiceProvider;
         this.mqttDeviceMqttService = mqttDeviceMqttService;
     }
@@ -42,6 +59,49 @@ public class MqttDeviceBootstrap implements IntegrationBootstrap {
 
     @Override
     public void onEnabled(String tenantId, Integration integrationConfig) {
+        log.info("Mqtt device integration starting");
+        initProperties();
+        subscribeTopics();
+        IntegrationBootstrap.super.onEnabled(tenantId, integrationConfig);
+        log.info("Mqtt device integration started");
+    }
+
+    private void initProperties() {
+        AnnotatedEntityWrapper<MqttDeviceIntegrationEntities.MqttDeviceProperties> wrapper = new AnnotatedEntityWrapper<>();
+        boolean isPropertiesInitialized = (boolean) wrapper.getValue(MqttDeviceIntegrationEntities.MqttDeviceProperties::getIsInitialized).orElse(false);
+        if (isPropertiesInitialized) {
+            return;
+        }
+        MqttBrokerInfo mqttBrokerInfo = mqttPubSubServiceProvider.getMqttBrokerInfo();
+        if (mqttBrokerInfo == null) {
+            throw ServiceException.with(ErrorCode.SERVER_ERROR.getErrorCode(), "Mqtt broker not found").build();
+        }
+        if (mqttBrokerInfo.getHost() == null) {
+            mqttBrokerInfo.setHost(getLocalAddress());
+        }
+        if (mqttBrokerInfo.getHost() == null || mqttBrokerInfo.getMqttPort() == null) {
+            throw ServiceException.with(ErrorCode.SERVER_ERROR.getErrorCode(), "Mqtt broker host or port empty").build();
+        }
+        Credentials mqttCredentials = credentialsServiceProvider.getOrCreateCredentials(CredentialsType.MQTT);
+        wrapper.saveValues(Map.of(
+                MqttDeviceIntegrationEntities.MqttDeviceProperties::getBrokerServer, mqttBrokerInfo.getHost(),
+                MqttDeviceIntegrationEntities.MqttDeviceProperties::getBrokerPort, mqttBrokerInfo.getMqttPort(),
+                MqttDeviceIntegrationEntities.MqttDeviceProperties::getBrokerUsername, mqttCredentials.getAccessKey(),
+                MqttDeviceIntegrationEntities.MqttDeviceProperties::getBrokerPassword, mqttCredentials.getAccessSecret(),
+                MqttDeviceIntegrationEntities.MqttDeviceProperties::getIsInitialized, true
+        ));
+    }
+
+    private String getLocalAddress() {
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+            return localHost.getHostAddress();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private void subscribeTopics() {
         List<Long> deviceTemplateIds = DataCenter.getDeviceTemplateIds();
         if (CollectionUtils.isEmpty(deviceTemplateIds)) {
             return;
@@ -56,6 +116,5 @@ public class MqttDeviceBootstrap implements IntegrationBootstrap {
             String topic = DataCenter.getTopic(deviceTemplate.getId());
             mqttDeviceMqttService.subscribe(topic, deviceTemplate.getId(), deviceTemplate.getContent());
         });
-        IntegrationBootstrap.super.onEnabled(tenantId, integrationConfig);
     }
 }
--- integrations/mqtt-device/src/main/java/com/milesight/beaveriot/integrations/mqttdevice/entity/MqttDeviceIntegrationEntities.java
+++ integrations/mqtt-device/src/main/java/com/milesight/beaveriot/integrations/mqttdevice/entity/MqttDeviceIntegrationEntities.java
@@ -5,6 +5,7 @@ import com.milesight.beaveriot.context.integration.context.DeleteDeviceAware;
 import com.milesight.beaveriot.context.integration.entity.annotation.Entities;
 import com.milesight.beaveriot.context.integration.entity.annotation.Entity;
 import com.milesight.beaveriot.context.integration.entity.annotation.IntegrationEntities;
+import com.milesight.beaveriot.context.integration.enums.AccessMod;
 import com.milesight.beaveriot.context.integration.enums.EntityType;
 import com.milesight.beaveriot.context.integration.model.ExchangePayload;
 import lombok.Data;
@@ -25,7 +26,10 @@ public class MqttDeviceIntegrationEntities extends ExchangePayload {
     public static final String UPDATE_DEVICE_TEMPLATE_IDENTIFIER = "update-device-template";
     public static final String DELETE_DEVICE_TEMPLATE_IDENTIFIER = "delete-device-template";
 
-    @Entity(type = EntityType.PROPERTY, identifier = TOPIC_MAP_IDENTIFIER, visible = false)
+    @Entity(type = EntityType.PROPERTY, name = "Mqtt device properties", accessMod = AccessMod.R)
+    private MqttDeviceProperties mqttDeviceProperties;
+
+    @Entity(type = EntityType.PROPERTY, identifier = TOPIC_MAP_IDENTIFIER, accessMod = AccessMod.R, visible = false)
     private String topicMap;
 
     @Entity(type = EntityType.SERVICE, identifier = ADD_DEVICE_IDENTIFIER, visible = false)
@@ -33,6 +37,26 @@ public class MqttDeviceIntegrationEntities extends ExchangePayload {
 
     @Entity(type = EntityType.SERVICE, identifier = DELETE_DEVICE_IDENTIFIER, visible = false)
     private DeleteDevice deleteDevice;
+
+    @Data
+    @EqualsAndHashCode(callSuper = true)
+    @Entities
+    public static class MqttDeviceProperties extends ExchangePayload {
+        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker properties initialized flag", visible = false)
+        private Boolean isInitialized;
+
+        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker server")
+        private String brokerServer;
+
+        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker port")
+        private Integer brokerPort;
+
+        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker username")
+        private String brokerUsername;
+
+        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker password")
+        private String brokerPassword;
+    }
 
     @Data
     @EqualsAndHashCode(callSuper = true)
