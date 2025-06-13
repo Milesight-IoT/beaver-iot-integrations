8b961642dcb8a767fd82c482a7adaf32e1af9d79
pandalxb <fzuxx@qq.com>
2025-06-13 14:45:49 +0800

    Remove property entities of integration

--- integrations/mqtt-device/src/main/java/com/milesight/beaveriot/integrations/mqttdevice/entity/MqttDeviceIntegrationEntities.java
+++ integrations/mqtt-device/src/main/java/com/milesight/beaveriot/integrations/mqttdevice/entity/MqttDeviceIntegrationEntities.java
@@ -23,9 +23,6 @@ public class MqttDeviceIntegrationEntities extends ExchangePayload {
     public static final String ADD_DEVICE_IDENTIFIER = "add-device";
     public static final String DELETE_DEVICE_IDENTIFIER = "delete-device";
 
-    @Entity(type = EntityType.PROPERTY, name = "Mqtt device properties", accessMod = AccessMod.R)
-    private MqttDeviceProperties mqttDeviceProperties;
-
     @Entity(type = EntityType.PROPERTY, identifier = TOPIC_MAP_IDENTIFIER, accessMod = AccessMod.R, visible = false)
     private String topicMap;
 
@@ -34,29 +31,6 @@ public class MqttDeviceIntegrationEntities extends ExchangePayload {
 
     @Entity(type = EntityType.SERVICE, identifier = DELETE_DEVICE_IDENTIFIER, visible = false)
     private DeleteDevice deleteDevice;
-
-    @Data
-    @EqualsAndHashCode(callSuper = true)
-    @Entities
-    public static class MqttDeviceProperties extends ExchangePayload {
-        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker properties initialized flag", visible = false)
-        private Boolean isInitialized;
-
-        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker server")
-        private String brokerServer;
-
-        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker port")
-        private Integer brokerPort;
-
-        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker username")
-        private String brokerUsername;
-
-        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker password")
-        private String brokerPassword;
-
-        @Entity(type = EntityType.PROPERTY, name = "Mqtt device broker topic prefix")
-        private String brokerTopicPrefix;
-    }
 
     @Data
     @EqualsAndHashCode(callSuper = true)
