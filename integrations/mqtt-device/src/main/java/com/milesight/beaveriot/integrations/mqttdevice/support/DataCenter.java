8fedd8f0f42e63b2e4f607eff56a82cf7271e73c
pandalxb <fzuxx@qq.com>
2025-06-17 21:18:24 +0800

    Modify: Remove tenantId from the topic map and load the topic map from the entity every time.

--- integrations/mqtt-device/src/main/java/com/milesight/beaveriot/integrations/mqttdevice/support/DataCenter.java
+++ integrations/mqtt-device/src/main/java/com/milesight/beaveriot/integrations/mqttdevice/support/DataCenter.java
@@ -3,11 +3,9 @@ package com.milesight.beaveriot.integrations.mqttdevice.support;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.milesight.beaveriot.base.utils.JsonUtils;
 import com.milesight.beaveriot.context.integration.wrapper.AnnotatedEntityWrapper;
-import com.milesight.beaveriot.context.security.TenantContext;
 import com.milesight.beaveriot.integrations.mqttdevice.entity.MqttDeviceIntegrationEntities;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * author: Luxb
@@ -17,53 +15,51 @@ public class DataCenter {
     public static final String INTEGRATION_ID = "mqtt-device";
     public static final String DEFAULT_DEVICE_KEY = "device_key";
     public static final String DEVICE_ID_PLACEHOLDER = "${device_id}";
-    private static final Map<String, Map<String, Long>> topicMap = loadTopicMap();
 
     public static void putTopic(String topic, Long deviceTemplateId) {
-        getTenantTopicMap().put(topic, deviceTemplateId);
-        saveTopicMap();
+        Map<String, Long> topicMap = loadTopicMap();
+        topicMap.put(topic, deviceTemplateId);
+        saveTopicMap(topicMap);
     }
 
     public static String getTopic(Long deviceTemplateId) {
-        return getTenantTopicMap().entrySet().stream().filter(entry -> entry.getValue().equals(deviceTemplateId)).map(Map.Entry::getKey).findFirst().orElse(null);
+        return loadTopicMap().entrySet().stream().filter(entry -> entry.getValue().equals(deviceTemplateId)).map(Map.Entry::getKey).findFirst().orElse(null);
     }
 
     public static Long getTemplateIdByTopic(String topic) {
-        for (String eachTopic : getTenantTopicMap().keySet()) {
+        Map<String, Long> topicMap = loadTopicMap();
+        for (String eachTopic : topicMap.keySet()) {
             String topicPattern = TopicSupporter.convert(eachTopic);
             if (TopicSupporter.matches(topicPattern, topic)) {
-                return getTenantTopicMap().get(eachTopic);
+                return topicMap.get(eachTopic);
             }
         }
         return null;
     }
 
     public static void removeTopic(String topic) {
-        getTenantTopicMap().remove(topic);
-        saveTopicMap();
+        Map<String, Long> topicMap = loadTopicMap();
+        topicMap.remove(topic);
+        saveTopicMap(topicMap);
     }
 
     public static void removeTopicByTemplateId(Long deviceTemplateId) {
-        getTenantTopicMap().entrySet().removeIf(entry -> entry.getValue().equals(deviceTemplateId));
-        saveTopicMap();
+        Map<String, Long> topicMap = loadTopicMap();
+        topicMap.entrySet().removeIf(entry -> entry.getValue().equals(deviceTemplateId));
+        saveTopicMap(topicMap);
     }
 
     public static boolean isTopicExist(String topic) {
-        return getTenantTopicMap().containsKey(topic);
+        return loadTopicMap().containsKey(topic);
     }
 
-    private static Map<String, Map<String, Long>> loadTopicMap() {
+    private static Map<String, Long> loadTopicMap() {
         AnnotatedEntityWrapper<MqttDeviceIntegrationEntities> entitiesWrapper = new AnnotatedEntityWrapper<>();
         String topicMapStr = (String) entitiesWrapper.getValue(MqttDeviceIntegrationEntities::getTopicMap).orElse("{}");
         return JsonUtils.fromJSON(topicMapStr, new TypeReference<>() {});
     }
 
-    private static Map<String, Long> getTenantTopicMap() {
-        String tenantId = TenantContext.getTenantId();
-        return topicMap.computeIfAbsent(tenantId, k -> new ConcurrentHashMap<>());
-    }
-
-    private static void saveTopicMap() {
+    private static void saveTopicMap(Map<String, Long> topicMap) {
         String topicMapStr = JsonUtils.toJSON(topicMap);
         AnnotatedEntityWrapper<MqttDeviceIntegrationEntities> entitiesWrapper = new AnnotatedEntityWrapper<>();
         entitiesWrapper.saveValue(MqttDeviceIntegrationEntities::getTopicMap, topicMapStr).publishSync();
